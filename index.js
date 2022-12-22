062d2698e2b3bcf0d8e75926a0ea8c00a7c25b29
shangida <shangida240@gmail.com>
2022-12-22 10:44:10 +0600

    Madam js

---
+++ index.js
@@ -0,0 +1,52 @@
+
+//whithout pramater
+
+/*var num1 = Number (prompt("Enter your Number:"));
+  var num2 = Number (prompt("Enter your second Number:"));
+  var num3 = Number (prompt("Enter your Thard Number:"));
+
+
+function squre ()
+{
+  
+  document.write("Add is: " + (num1+num2+num3) + "<br/>" );
+  document.write("sub is: " + (num1-num2-num3) + "<br/>");
+  document.write("Mul is: " + (num1*num2*num3) + "<br/>");
+  document.write("Division is" + (num1 / num2/ num3) + "<br/>");
+
+}
+squre()*/
+
+var num1 = Number (prompt("Enter your Number:"));
+var num2 = Number (prompt("Enter your second Number:"));
+var num3 = Number (prompt("Enter your Thard Number:"));
+
+  function mathmatics(num1, num2, num3)
+  {
+    document.write("Add is: " + (num1+num2+num3) + "<br/>" );
+    document.write("sub is: " + (num1-num2-num3) + "<br/>");
+    document.write("Mul is: " + (num1*num2*num3) + "<br/>");
+    document.write("Division is" + (num1 / num2/ num3) + "<br/>");
+  
+  }
+
+  mathmatics(num1, num2, num3);
+
+
+
+/*function squre()
+{
+    var num = 16;
+    document.write(" Result is " + num * num);
+}
+squre();*/
+
+
+
+/*function squre ()
+{
+
+    var num = 16;
+    console.log(num* num);
+}
+squre()*/
